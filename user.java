f27a0f91053914fc361ff2fe5047247ba63ffe70
RohiniGore <rohini.gore.dev@gmail.com>
2025-08-02 21:44:37 +0530

    Update user.java

--- user.java
+++ user.java
@@ -2,7 +2,7 @@ class User{
 public static void main (String []args){
  System.out.println("Welcome to user class");
   System.out.println("Welcome to GAP project"); 
- 
+ int i = 30;
  String s = "java";
 }
 }
